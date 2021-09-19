a10479f8115013bd0b0e4252e3cafcbfbb8c5a6d
tkssharma <tarun.softengg@gmail.com>
2021-09-19 14:04:28 +0530

    react tailwind setup poc

--- react-tailwind-js/src/App.js
+++ react-tailwind-js/src/App.js
@@ -1,14 +1,17 @@
+
+import About from './components/about';
+import Card from './components/card';
+import Skills from './components/skills';
+import Footer from './components/footer';
 function App() {
   return (
-    <div className="bg-blue-400 h-screen grid">
-      <div className="w-3/4 my-auto ml-20">
-        <h1 className="text-5xl font-bold mb-10 text-white">JIT mode is cool</h1>
-        <p className="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo officia earum ducimus neque obcaecati consequuntur ratione accusamus at officiis tempore,
-          magnam non debitis fugit unde alias id quidem necessitatibus.
-        </p>
-      </div>
+    <div className="py-10 min-h-screen bg-gray-100 dm:px-5 min-h-screen">
+      <Card />
+      <About />
+      <Skills />
+      <Footer />
     </div>
+
   );
 }
 
